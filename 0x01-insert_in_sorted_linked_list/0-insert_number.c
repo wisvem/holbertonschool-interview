c788214e1fb25872dca746fa8a412b05b999e93e
Wiston Venera Macías <wisvem@hotmail.com>
2021-07-27 12:18:16 +0000

    Task done

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * insert_node - function to insert node sorted
+ * @head: list head
+ * @number: number to insert
+ * Return: a new node
+ **/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *h;
+	listint_t *new = NULL;
+	listint_t *prev;
+
+	if (head != NULL)
+	{
+		h = *head;
+		prev = *head;
+		while (h != NULL && (number > (*h).n))
+		{
+			prev = h;
+			h = (*h).next;
+		}
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		(*new).n = number;
+		(*new).next = NULL;
+		(*new).n = number;
+		(*new).next = (*prev).next;
+		(*prev).next = new;
+	}
+	return (new);
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,9 @@
+# 0x01. Insert in sorted linked list
+
+Write a function in C that inserts a number into a sorted singly linked list.
+
+Prototype: listint_t *insert_node(listint_t **head, int number);
+Return: the address of the new node, or NULL if it failed
+
+# Contributors
+- [@wisvem](https://www.github.com/wisvem) - Wiston Venera Macías
