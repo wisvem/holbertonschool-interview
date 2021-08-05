320df8fcee526054f582da6d82f9c29ccaad19bf
Wiston Venera Macías <wisvem@hotmail.com>
2021-08-05 01:34:22 +0000

    File creation

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert - entry point
+ * @root: root of tree
+ * @value: value to insert
+ * Return: return new nodes
+**/
+heap_t *heap_insert(heap_t **root, int value)
+{
+	return (NULL);
+}
