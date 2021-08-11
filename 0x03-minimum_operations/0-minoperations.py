fb95ce8d7f4131e4b72bb923398a68532b885e89
Wiston Venera Macías <wisvem@hotmail.com>
2021-08-11 04:56:25 +0000

    Code refactored: removed is_prime function

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -2,40 +2,13 @@
 """Minumun operations module"""
 
 
-def is_prime(n):
-    """Prime function"""
-    if n == 2 or n == 3:
-        return True
-    if n < 2 or n % 2 == 0:
-        return False
-    if n < 9:
-        return True
-    if n % 3 == 0:
-        return False
-    r = int(n**0.5)
-    f = 5
-    while f <= r:
-        #        print('\t', f)
-        if n % f == 0:
-            return False
-        if n % (f+2) == 0:
-            return False
-        f += 6
-    return True
-
-
 def minOperations(n):
     """Min op function"""
-    copy = n
     operations = 0
-    while copy > 1:
-        if is_prime(copy):
-            operations += copy
-            copy = int(copy / copy)
-        else:
-            divisor = min_divisor(copy)
-            operations += divisor
-            copy = int(copy / divisor)
+    while n > 1:
+        divisor = min_divisor(n)
+        operations += divisor
+        n = int(n / divisor)
     return int(operations)
 
 
