064724364a58ae9994ef881ed2182bf8a6a5487b
Wiston Venera Macías <wisvem@hotmail.com>
2021-08-31 21:20:24 +0000

    Task done

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from re import search
+from sys import stdin, stdout
+
+
+def main():
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    count = 1
+    file_size = 0
+    regex = r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b - \[[0-9]{4}-[0-1]{1}[0-9]-{1}[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}\] "GET \/projects\/260 HTTP\/1.1" [0-9]{3} [0-9]{1,4}$'
+    try:
+        for line in stdin:
+            if search(regex, line):
+                file_size += int(line.split(' ')[-1])
+                code = int(line.split(' ')[-2])
+                if code in codes:
+                    codes[int(line.split(' ')[-2])] += 1
+            if count is 10:
+                print(f'File size: {file_size}')
+                for k, v in codes.items():
+                    if v is not 0:
+                        print(f'{k}: {v}')
+                count = 1
+                file_size = 0
+            else:
+                count += 1
+    except KeyboardInterrupt:
+        print(f'File size: {file_size}')
+        for k, v in codes.items():
+            if v is not 0:
+                print(f'{k}: {v}')
+
+if __name__ == "__main__":
+    main()
