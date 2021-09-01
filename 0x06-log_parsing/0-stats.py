be51c4251634110c3baabc394c59777ab28fec6f
Wiston Venera Macías <wisvem@hotmail.com>
2021-09-01 01:24:03 +0000

    Refactored

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,11 +5,10 @@ from sys import stdin, stdout
 
 
 def main():
-    """Main function
-    """
+    """Main function"""
+    count = 1
     code_list = ["200", "301", "400", "401", "403", "404", "405", "500"]
     codes = dict.fromkeys(code_list, 0)
-    count = 1
     file_size = 0
 
     try:
@@ -22,23 +21,22 @@ def main():
             if code in codes:
                 codes[line.split(' ')[-2]] += 1
             if count is 10:
-                print('File size: {}'.format(file_size))
-                for code in code_list:
-                    if codes[code] is not 0:
-                        print('{}: {}'.format(code, codes[code]))
+                print_status(file_size, code_list, codes)
                 count = 1
             else:
                 count += 1
     except KeyboardInterrupt:
-        print('File size: {}'.format(file_size))
-        for code in code_list:
-            if codes[code] is not 0:
-                print('{}: {}'.format(code, codes[code]))
+        print_status(file_size, code_list, codes)
     if count < 10:
-        print('File size: {}'.format(file_size))
-        for code in code_list:
-            if codes[code] is not 0:
-                print('{}: {}'.format(code, codes[code]))
+        print_status(file_size, code_list, codes)
+
+
+def print_status(fs, cl, codes):
+    """Print function"""
+    print('File size: {}'.format(fs))
+    for code in cl:
+        if codes[code] is not 0:
+            print('{}: {}'.format(code, codes[code]))
 
 
 if __name__ == "__main__":
