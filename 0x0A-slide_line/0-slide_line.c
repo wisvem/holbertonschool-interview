3d859fdd268da8ab53e4ea27b9adf8ac14243a93
Wiston Venera Macías <wisvem@hotmail.com>
2021-09-08 21:05:21 +0000

    First try

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,116 @@
+#include "slide_line.h"
+
+/**
+* move_left - move left
+* @size2: array size
+* @line: array
+* Return: 1 ok, 0 no
+**/
+void move_left(int *line, int size2)
+{
+	int i, j = 0, sw = 0;
+
+	for (i = 0; i < size2; i++)
+	{
+		if (line[i] != 0)
+		{
+			line[j] = line[i];
+			j++;
+		}
+	}
+	for (i = 0; i < size2 - 1; i++)
+	{
+		if (i < j && line[i] == line[i + 1])
+		{
+			sw = 1;
+			line[i] += line[i + 1];
+			line[i + 1] = 0;
+		}
+		else if (i >= j)
+		{
+			line[i] = 0;
+		}
+	}
+	if (sw == 1)
+		line[size2 - 1] = 0;
+	j = 0;
+	for (i = 0; i < size2; i++)
+	{
+		if (line[i] != 0)
+		{
+			line[j] = line[i];
+			j++;
+		}
+	}
+	for (i = j; i < size2; i++)
+		line[i] = 0;
+}
+/**
+* move_right - move right
+* @size2: array size
+* @line: array
+* Return: 1 ok, 0 no
+**/
+void move_right(int *line, int size2)
+{
+	int i, j = size2 - 1, sw = 0;
+
+	for (i = size2 - 1; i >= 0; i--)
+	{
+		if (line[i] != 0)
+		{
+			line[j] = line[i];
+			j--;
+		}
+	}
+	for (i = size2 - 1; i >= 0; i--)
+	{
+		if (i > j && line[i] == line[i - 1])
+		{
+			sw = 1;
+			line[i] += line[i - 1];
+			line[i - 1] = 0;
+		}
+		else if (i <= j)
+		{
+			line[i] = 0;
+		}
+	}
+	if (sw == 1)
+		line[0] = 0;
+	j = size2 - 1;
+	for (i = size2 - 1; i >= 0; i--)
+	{
+		if (line[i] != 0)
+		{
+			line[j] = line[i];
+			j--;
+		}
+	}
+	for (i = 0; i <= j; i++)
+		line[i] = 0;
+}
+/**
+* slide_line - entry point
+* @size: array size
+* @direction: direction
+* @line: array
+* Return: 1 ok, 0 no
+**/
+int slide_line(int *line, size_t size, int direction)
+{
+	int sw = 0, size2 = size;
+
+	if (direction == 1)
+	{
+		move_left(line, size2);
+		sw = 1;
+	}
+	else if (direction == 0)
+	{
+		move_right(line, size2);
+		sw = 1;
+	}
+
+	return (sw);
+}
---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,10 @@
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
+
+#include <stdio.h>
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT 0
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* SLIDE_LINE_H */
