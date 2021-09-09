ed5cf21c922bc4427228e09003496d99e0159569
Wiston Venera Macías <wisvem@hotmail.com>
2021-09-09 05:15:22 +0000

    Changed move left function

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -2,51 +2,42 @@
 
 /**
 * move_left - move left
-* @size2: array size
+* @size: array size
 * @line: array
 * Return: 1 ok, 0 no
 **/
-void move_left(int *line, int size2)
+void move_left(int *line, size_t size)
 {
-	int i, j = 0, sw = 0;
+	size_t i = 0, j = 0, k = 0;
 
-	for (i = 0; i < size2; i++)
+	for (i = 0, j = 0; i < size;)
 	{
+		if (line[i] == 0)
+			for (; line[i] == 0;)
+				i++;
+		if (i == (size - 1))
+			break;
+		j = i + 1;
+		if (line[j] == 0)
+			for (; line[j] == 0;)
+				j++;
+		if (line[i] == line[j])
+		{
+			line[i] = line[i] + line[j];
+			line[j] = 0;
+			i = j + 1;
+		}
+		else
+			i++;
+		j = 0;
+	}
+	for (i = 0, k = 0; i < size; i++)
 		if (line[i] != 0)
-		{
-			line[j] = line[i];
-			j++;
-		}
-	}
-	if (j < size2)
-		line[j] = 0;
-	for (i = 0; i < size2 - 1; i++)
-	{
-		if (i < j && line[i] == line[i + 1])
-		{
-			sw = 1;
-			line[i] += line[i + 1];
-			line[i + 1] = 0;
-		}
-		else if (i >= j)
-		{
-			line[i] = 0;
-		}
-	}
-	if (sw == 1 && j < size2)
-		line[size2 - 1] = 0;
-	j = 0;
-	for (i = 0; i < size2; i++)
-	{
-		if (line[i] != 0)
-		{
-			line[j] = line[i];
-			j++;
-		}
-	}
-	for (i = j; i < size2; i++)
-		line[i] = 0;
+			line[k++] = line[i];
+	while (k < size)
+		line[k++] = 0;
 }
+
 /**
 * move_right - move right
 * @size2: array size
@@ -105,15 +96,11 @@ int slide_line(int *line, size_t size, int direction)
 {
 	int size2 = size;
 
-	if (direction != 0 && direction != 1)
-		return (0);
-	if (line == NULL || size < 1)
-		return (0);
-	if (direction == 1)
+	if (direction == SLIDE_LEFT)
 	{
 		move_left(line, size2);
 	}
-	else if (direction == 0)
+	else if (direction == SLIDE_RIGHT)
 	{
 		move_right(line, size2);
 	}
