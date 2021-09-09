aed016d48cd7a9800e347262171223b26b5719cd
Wiston Venera Macías <wisvem@hotmail.com>
2021-09-09 04:55:58 +0000

    j = 0

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -18,7 +18,7 @@ void move_left(int *line, int size2)
 			j++;
 		}
 	}
-
+	line[j] = 0;
 	for (i = 0; i < size2 - 1; i++)
 	{
 		if (i < j && line[i] == line[i + 1])
@@ -64,7 +64,7 @@ void move_right(int *line, int size2)
 			j--;
 		}
 	}
-
+	line[j] = 0;
 	for (i = size2 - 1; i >= 0; i--)
 	{
 		if (i > j && line[i] == line[i - 1])
