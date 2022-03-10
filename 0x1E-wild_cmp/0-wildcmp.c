bb51ed9251fcc7436cec71d0bea0d222fbde5e12
Wiston Venera Macías <wisvem@hotmail.com>
2022-03-10 15:25:08 -0500

    Refactoring

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,65 +1,29 @@
 #include "holberton.h"
 
 /**
-* _strlen_recursion - returns the length of a string
-* @s: string
-* Return: lenght of @s
-**/
-int _strlen_recursion(char *s)
-{
-	if (*s != '\0')
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
-	else
-	{
-		return (0);
-	}
-}
-
-/**
-* comparar - compare two string
-* @s1: string 1
-* @s2: string 2
-* Return: 1 for equal, 0 different
-**/
-int comparar(char *s1, char *s2)
-{
-	if (*s1 == '\0')
-	{
-		return (1);
-	}
-	if (*s1 == *s2 || *s1 == '*' || *s2 == '*')
-	{
-		return (1 * comparar(s1 + 1, s2 + 1));
-	}
-}
-
-/**
-* wildcmp - comare two string
-* @s1: string 1
-* @s2: string 2
-* Return: 1 similar, 0 not similar
-**/
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise return 0.
+ * @s1: string to compare to
+ * @s2: string with wild character
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int wildcmp(char *s1, char *s2)
 {
-	int ls1 = _strlen_recursion(s1);
-	int ls2 = _strlen_recursion(s2);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-	if (ls1 != ls2)
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
 	{
-		return (0);
-	}
-	else
-	{
-		ls2 = (comparar(s1, s2));
-		if (ls2 == 1)
-		{
-			return (ls2);
-		}
-		else
-		{
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 			return (0);
-		}
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
 	}
+
+	return (0);
 }
